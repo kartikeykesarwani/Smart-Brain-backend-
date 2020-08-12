551840111bc9594bc6561238375c3eb5aad1ce55
kartikeykesarwani <kartikeykesarwani.2@gmail.com>
2020-08-13 00:41:38 +0530

    adding main file

--- server.js
+++ server.js
@@ -66,14 +66,11 @@ app.post('/findface',(req,res) => {
 
 app.post('/register',(req,res) => {
   const { email,name,password } = req.body;
-  database.users.push({
-    id:'125',
-       name: name ,
-       email: email ,
-       password: password,
-       entries: 0,
-       joined : new Data()
-  })
+  db('users').insert({
+    email: email,
+    name : name,
+    joined : new Date()
+  }).then(console.log)
   res.json(database.users[database.users.length - 1])
 })
 
